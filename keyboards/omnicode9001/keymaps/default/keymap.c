fb8ac7e7f67d4c36080abcc4a42817f7fab8af4e
Allen Choi <37539914+Thunderbird2086@users.noreply.github.com>
2025-07-24 01:02:26 +0900

    OmniCode:9001 - detect OS and swap CMD/OPT

--- keyboards/omnicode9001/keymaps/default/keymap.c
+++ keyboards/omnicode9001/keymaps/default/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS,     KC_DEL , KC_END , KC_PGDN,     KC_P7  , KC_P8  , KC_P9  , KC_PPLS,
 		KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,          KC_ENT ,                                    KC_P4  , KC_P4  , KC_P4  ,
 		KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH,                   KC_RSFT,              KC_UP  ,              KC_P1  , KC_P2  , KC_P3  , KC_PENT,
-		KC_LCTL, KC_LCMD, KC_LOPT,                   KC_SPC ,                            KC_ROPT, KC_RCMD, MO(2)  ,          KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT,     KC_P0  ,          KC_PDOT
+		KC_LCTL, KC_LOPT, KC_LCMD,                   KC_SPC ,                            KC_RCMD, KC_ROPT, MO(2)  ,          KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT,     KC_P0  ,          KC_PDOT
 	),
 
 	[1] = LAYOUT(
@@ -42,3 +42,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+#if defined(OS_DETECTION_ENABLE)
+uint32_t swap_cmd_opt(uint32_t trigger_time, void *cb_arg) {
+  os_variant_t host = detected_host_os();
+  uint16_t retry_ms = 500;
+
+  if (host == OS_MACOS || host == OS_IOS) {
+    keymap_config.swap_lalt_lgui = true;
+    keymap_config.swap_ralt_rgui = true;
+    retry_ms = 0;
+  }
+
+  return retry_ms;
+}
+
+void keyboard_post_init_user(void) {
+  defer_exec(100, swap_cmd_opt, NULL);
+}
+#endif // OS_DETECTION_ENABLE
